f4e9b811ff9efcd19729dc59be895c6b4f858491
Taketo-Muroya <54876706+Taketo-Muroya@users.noreply.github.com>
2022-01-21 15:28:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,8 @@ st.write(f"""
 # Set keyword ("失業" = "unemployment")
 pytrends.build_payload(kw, timeframe='2004-01-01 2021-11-30', geo='JP')
 gt1 = pytrends.interest_over_time()
+
+st.table(gt1)
 st.line_chart(gt1.iloc[:,1])
 #gt1 = gt1.rename(columns = {"失業": "unemployment", "isPartial": "info"})
 #gt1.to_csv("gt1.csv")
