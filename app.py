7bf8fc820b2b8fedb850445d26c60956d05b5097
Taketo-Muroya <54876706+Taketo-Muroya@users.noreply.github.com>
2022-01-21 15:12:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,12 @@ pytrends = TrendReq(hl='ja-JP', tz=360)
 
 st.title('グーグルトレンドによる景気ナウキャスティング')
 
+#@st.cache
+ibc = pd.read_csv('ibc_new.csv')
+ibc['Coincident ann'] = 100*ibc['Coincident Index'].pct_change(12)
+st.table(ibc.tail(10))
+st.line_chart(ibc['Coincident Index'])
+
 st.sidebar.write("""
 # GAFA株価
 こちらは株価可視化ツールです。以下のオプションから表示日数を指定できます。
@@ -25,12 +31,6 @@ st.write(f"""
 ### 過去 **{kw}日間** のGAFA株価
 """)
 
-#@st.cache
-ibc = pd.read_csv('ibc_new.csv')
-ibc['Coincident ann'] = 100*ibc['Coincident Index'].pct_change(12)
-st.table(ibc.tail(10))
-st.line_chart(ibc['Coincident Index'])
-
 # Set keyword ("失業" = "unemployment")
 pytrends.build_payload(kw, timeframe='2004-01-01 2021-11-30', geo='JP')
 gt1 = pytrends.interest_over_time()
@@ -39,34 +39,3 @@ st.line_chart(gt1.iloc[:,0])
 #gt1.to_csv("gt1.csv")
 #dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m-%d')
 #gt1 = pd.read_csv('gt1.csv', index_col=0, date_parser=dateparse, dtype='float')
-
-# Extract trend factor
-s1 = seasonal_decompose(gt1.iloc[:,0], extrapolate_trend='freq')
-t1 = s1.trend
-st.line_chart(t1)
-st.line_chart(gt1.iloc[:,0])
-#plt.plot(t1)
-#plt.plot(gt1.iloc[:,0], linestyle='--')
-
-# Check correlation
-level = ibc['Coincident Index'][228:]
-level.index = t1.index
-cor = level.corr(t1)
-st.write("Correlation of level: {:.2f}".format(cor))
-#print("Correlation of level: {:.2f}".format(cor))
-
-a1 = gt1.iloc[:,0].pct_change(12)
-ann = ibc['Coincident ann'][228:]
-ann.index = a1.index
-cor = ann.corr(a1)
-st.write("Correlation of YoY: {:.2f}".format(cor))
-#print("Correlation of YoY: {:.2f}".format(cor))
-
-# Plot trend
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-ax.plot(t1.index, ibc['Coincident Index'][228:], linestyle='-', color='b', label='IBC')
-ax.plot(t1.index, t1, linestyle='--', color='#e46409', label='google search: "unemployment"')
-ax.legend()
-plt.title('Google Search: "Unemployment"')
-#plt.savefig("google1.png")
