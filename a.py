9a3ce78b45892ec55ec291253599862023aca6e5
Taketo-Muroya <54876706+Taketo-Muroya@users.noreply.github.com>
2022-01-21 14:01:15 +0900

    progress first

--- a.py
+++ a.py
@@ -6,6 +6,21 @@ from PIL import Image
 
 st.title("Streamlit 超入門")
 
+# Progress Bar
+
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+import time
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i+1)
+    time.sleep(0.1)
+
+'Done!!'
+
 st.write("DataFrame")
 
 df = pd.DataFrame({
@@ -95,17 +110,3 @@ ex.write('問い合わせ内容を書く')
 ex.write('問い合わせ内容を書く')
 ex.write('問い合わせ内容を書く')
 
-# Progress Bar
-
-#'Start!!'
-
-#latest_iteration = st.empty()
-#bar = st.progress(0)
-
-#import time
-#for i in range(100):
-#    latest_iteration.text(f'Iteration {i+1}')
-#    bar.progress(i+1)
-#    time.sleep(0.1)
-
-#'Done!!'
