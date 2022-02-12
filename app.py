f6c680f1234d126b4fdc08f6c8f7fb9f47df0347
Taketo-Muroya <54876706+Taketo-Muroya@users.noreply.github.com>
2022-02-12 22:36:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -90,13 +90,10 @@ st.title('景気ナウキャスティング')
 
 ibc = pd.read_csv('data/ibc_new.csv')
 ibc['Coincident ann'] = 100*ibc['Coincident Index'].pct_change(12)
-#st.table(ibc.tail(10))
-#st.line_chart(ibc['Coincident Index'])
 
-#st.sidebar.write("""
-## GAFA株価
-#こちらは株価可視化ツールです。以下のオプションから表示日数を指定できます。
-#""")
+st.sidebar.write("""
+こちらはGoogleトレンドによる景気予測ツールです。検索ワードを記入してください。
+""")
 
 kw1 = st.sidebar.text_input('検索ワードを記入してください', '失業')
 kw2 = st.sidebar.text_input('検索ワードを記入してください', '貯金')
@@ -110,32 +107,24 @@ st.write(f"""
 kw_list1 = [kw1]
 pytrends.build_payload(kw_list1, timeframe='2004-01-01 2021-11-30', geo='JP')
 gt1 = pytrends.interest_over_time()
-
 gt1 = gt1.rename(columns = {kw1:"var1", "isPartial":"info"})
-#gt1.to_csv("gt1.csv")
-#dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m-%d')
-#gt1 = pd.read_csv('gt1.csv', index_col=0, date_parser=dateparse, dtype='float')
 
 # Extract trend factor
 t1 = seasonal_decompose(gt1.iloc[:,0], extrapolate_trend='freq').trend
 data1 = pd.merge(gt1.iloc[:,0], t1, on='date')
 st.line_chart(data1)
-#plt.plot(t1)
-#plt.plot(gt1.iloc[:,0], linestyle='--')
 
 # Check correlation
 level = ibc['Coincident Index'][228:]
 level.index = t1.index
 cor = level.corr(t1)
 st.write("Correlation of level: {:.2f}".format(cor))
-#print("Correlation of level: {:.2f}".format(cor))
 
 a1 = gt1.iloc[:,0].pct_change(12)
 ann = ibc['Coincident ann'][228:]
 ann.index = a1.index
 cor = ann.corr(a1)
 st.write("Correlation of YoY: {:.2f}".format(cor))
-#print("Correlation of YoY: {:.2f}".format(cor))
 
 st.write(f"""
 ### **「{kw2}」** のグーグルトレンド
@@ -147,16 +136,11 @@ kw_list2 = [kw2]
 pytrends.build_payload(kw_list2, timeframe='2004-01-01 2021-11-30', geo='JP')
 gt2 = pytrends.interest_over_time()
 gt2 = gt2.rename(columns = {kw2:"var2", "isPartial":"info"})
-#gt2.to_csv("data/gt2.csv")
-#dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m-%d')
-#gt2 = pd.read_csv('data/gt2.csv', index_col=0, date_parser=dateparse, dtype='float')
 
 # Extract trend factor
 t2 = seasonal_decompose(gt2.iloc[:,0], extrapolate_trend='freq').trend
 data2 = pd.merge(gt2.iloc[:,0], t2, on='date')
 st.line_chart(data2)
-#plt.plot(t2)
-#plt.plot(gt2.iloc[:,0], linestyle='--')
 
 # Check correlation
 level = ibc['Coincident Index'][228:]
