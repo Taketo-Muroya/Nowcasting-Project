a56fae34222803f7f40441b663e03acd4fe39f08
Taketo-Muroya <54876706+Taketo-Muroya@users.noreply.github.com>
2022-02-13 00:29:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def google_trend(kw):
 
   # Extract trend factor
   t = seasonal_decompose(gt.iloc[:,0], extrapolate_trend='freq').trend
-  #t = pd.DataFrame(t).rename(columns = {"trend":f'{kw}-trend'})
+  t = pd.DataFrame(t).rename(columns = {"trend":kw})
   a = gt.iloc[:,0].pct_change(12)
   #a = pd.DataFrame(a).rename(columns = {kw:"YoY"})
   temp = pd.merge(gt.iloc[:,0], t, on='date')
@@ -141,9 +141,6 @@ st.write("Correlation of YoY: {:.2f}".format(cor_ann2))
 #gtrend_y = pd.concat([a1, a2], axis=1).rename(columns={'var1': 'var1_rate', 'var2': 'var2_rate'})
 
 # Set time series dataset
-
-st.dataframe(data1.iloc[:,1])
-
 gtrend_l = pd.concat([data1.iloc[:,1], data2.iloc[:,1]], axis=1)
 gtrend_y = pd.concat([data1.iloc[:,2], data2.iloc[:,2]], axis=1)
 X = pd.merge(gtrend_l, gtrend_y, on='date')
