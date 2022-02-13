9d2bc6dc667cb8a050abde342d1084e58829d031
Taketo-Muroya <54876706+Taketo-Muroya@users.noreply.github.com>
2022-02-13 11:47:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -180,6 +180,57 @@ def model_eval_metrics(y_true, y_pred, classification="TRUE"):
         finalmetricdata = pd.DataFrame.from_dict(metricdata)
      return finalmetricdata
 
+def nowcasting(XX):
+
+  # feature scaling
+  END = len(XX)-XX['ibc'].isnull().sum()
+  dataset = XX.iloc[:END,:].values
+  data_mean = dataset.mean(axis=0)
+  data_std = dataset.std(axis=0)
+  dataset = (dataset-data_mean)/data_std
+
+  # create the test data
+  past_history = 3
+  future_target = 0
+  STEP = 1
+  x_single, y_single = multivariate_data(dataset, dataset[:,0], 0, None, past_history, future_target, STEP, single_step=True)
+
+  # save the output
+  past_estimate = pd.DataFrame(single_step_model.predict(x_single)*data_std[0]+data_mean[0])
+  past_estimate.index = XX.iloc[past_history:END,:].index
+
+  # visualize the result 
+  st.line_chart(past_estimate)
+
+  # nowcast the future IBC
+  for i in range(END, len(XX)):
+    XX.iat[i,0] = float(single_step_model.predict(x_single)[-1]*data_std[0]+data_mean[0])
+    #XX.iat[i,0] = XX.iat[i-1,0]
+    temp = XX.iloc[:i+1,:]
+    st.write(temp.tail())
+    st.write('-----------------------------------------------')
+
+    # feature scaling
+    dataset = temp.values
+    data_mean = dataset.mean(axis=0)
+    data_std = dataset.std(axis=0)
+    dataset = (dataset-data_mean)/data_std
+    
+    # create the test data
+    x_single, y_single = multivariate_data(dataset, dataset[:,0], 0, None, past_history, future_target, STEP, single_step=True)
+
+    XX.iat[i,0] = float(single_step_model.predict(x_single)[-1]*data_std[0]+data_mean[0])
+    st.write(XX.tail(10))
+    st.write('-----------------------------------------------')
+
+  # save the output
+  future_estimate = pd.DataFrame(XX.iloc[END:len(XX)+1,0])
+
+  #plt.plot(single_step_model.predict(x_single)*data_std[0]+data_mean[0], "r", linestyle='--', label="predict")
+  df_concat = pd.concat([past_estimate.set_axis(['ibc'], axis='columns'), future_estimate])
+
+  return df_concat
+
 ibc = pd.read_csv('data/ibc_new.csv')
 ibc['Coincident ann'] = 100*ibc['Coincident Index'].pct_change(12)
 
@@ -233,52 +284,7 @@ if st.button('推計開始'):
   temp = pd.merge(df1.iloc[:,1], df2.iloc[:,1], on='date')
   XX = pd.merge(wibc, temp, on='date')
 
-  # feature scaling
-  END = len(XX)-XX['ibc'].isnull().sum()
-  dataset = XX.iloc[:END,:].values
-  data_mean = dataset.mean(axis=0)
-  data_std = dataset.std(axis=0)
-  dataset = (dataset-data_mean)/data_std
-
-  # create the test data
-  past_history = 3
-  future_target = 0
-  STEP = 1
-  x_single, y_single = multivariate_data(dataset, dataset[:,0], 0, None, past_history, future_target, STEP, single_step=True)
-
-  # save the output
-  past_estimate = pd.DataFrame(single_step_model.predict(x_single)*data_std[0]+data_mean[0])
-  past_estimate.index = XX.iloc[past_history:END,:].index
-
-  # visualize the result 
-  st.line_chart(past_estimate)
-
-  # nowcast the future IBC
-  for i in range(END, len(XX)):
-    XX.iat[i,0] = float(single_step_model.predict(x_single)[-1]*data_std[0]+data_mean[0])
-    #XX.iat[i,0] = XX.iat[i-1,0]
-    temp = XX.iloc[:i+1,:]
-    st.write(temp.tail())
-    st.write('-----------------------------------------------')
-
-    # feature scaling
-    dataset = temp.values
-    data_mean = dataset.mean(axis=0)
-    data_std = dataset.std(axis=0)
-    dataset = (dataset-data_mean)/data_std
-    
-    # create the test data
-    x_single, y_single = multivariate_data(dataset, dataset[:,0], 0, None, past_history, future_target, STEP, single_step=True)
-
-    XX.iat[i,0] = float(single_step_model.predict(x_single)[-1]*data_std[0]+data_mean[0])
-    st.write(XX.tail(10))
-    st.write('-----------------------------------------------')
-
-  # save the output
-  future_estimate = pd.DataFrame(XX.iloc[END:len(XX)+1,0])
-
-  #plt.plot(single_step_model.predict(x_single)*data_std[0]+data_mean[0], "r", linestyle='--', label="predict")
-  df_concat = pd.concat([past_estimate.set_axis(['ibc'], axis='columns'), future_estimate])
-  st.line_chart(df_concat)
+  result = nowcasting(XX)
+  st.line_chart(result)
 
   comment.write('推計が完了しました')
