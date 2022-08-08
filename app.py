86aa03c6ec3c4e770eccf77b350ba67951a613e4
Taketo Muroya <roomroof@gmail.com>
2022-08-08 17:48:17 +0900

    add

--- app.py
+++ app.py
@@ -301,10 +301,11 @@ st.dataframe(ts)
 # Plot trend
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-ax.plot(data1.index, ts['Coincident Index'], linestyle='-', color='b', label='IBC')
-ax.plot(data1.index, data1.iloc[:,0], linestyle='--', color='#e46409', label='google search: "unemployment"')
+ax.plot(ts.index, ts['Coincident Index'], linestyle='-', color='b', label='IBC')
+ax.plot(ts.index, ts['Coincident Index'], linestyle='--', color='#e46409', label='google search: "unemployment"')
 ax.legend()
 plt.title('Google Search: "Unemployment"')
+
 st.pyplot(fig)
 
 
