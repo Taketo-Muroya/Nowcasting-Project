d4401cf5cd3db9b6445a3c362853f2a20cee09b8
Taketo Muroya <roomroof@gmail.com>
2022-08-10 17:41:42 +0900

    a

--- app.py
+++ app.py
@@ -329,7 +329,7 @@ st.write("#####  ")
 ts = ts[ts.index > datetime.datetime(2010, 1, 1)]
 #ts.index = ts.index.datatime.date()
 #ts.index = pd.to_datetime(ts.index).date()
-st.dataframe(ts.index.datetime.date())
+st.dataframe(datatime.date(ts.index))
 
 # 推計 -------------------------------------------------------------------------------------
 if st.button('推計開始'):
