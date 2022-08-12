3587494f3e9c5ea23a4025350cbcd95ec00500e5
Taketo Muroya <roomroof@gmail.com>
2022-08-12 15:26:00 +0900

    a

--- app.py
+++ app.py
@@ -289,6 +289,7 @@ y = ibc[228:]
 y = y.set_index('time')
 y.index = X[:len(ibc)-228].index
 ts = pd.merge(y, X, on='date')
+st.dataframe(ts)
 ts.index = datetime.datetime.strptime(ts.index, '%Y/%m/%d %H:%M:%S').strftime('%Y/%m/%d')
 #gt['date'] = pd.to_datetime(gt['date']).datetime.date()
 st.dataframe(ts)
