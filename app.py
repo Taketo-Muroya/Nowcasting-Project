0ab2b6838c300678cd5bcaa042872c6b33a13e32
Taketo Muroya <roomroof@gmail.com>
2022-08-12 15:38:01 +0900

    a

--- app.py
+++ app.py
@@ -289,10 +289,10 @@ y = ibc[228:]
 y = y.set_index('time')
 y.index = X[:len(ibc)-228].index
 ts = pd.merge(y, X, on='date')
-st.write(ts.index.iloc[0,0])
+ts.reset_index()
 #new_date, new_time = ts.index.split()
 #ts.index = datetime.datetime.strptime(ts.index, '%Y-%m-%d %H:%M:%S').strftime('%Y/%m/%d')
-ts.index[0] = pd.to_datetime(ts.index[0]).datetime.date()
+#ts.index[0] = pd.to_datetime(ts.index[0]).datetime.date()
 st.dataframe(ts)
 
 # データ期間の設定
