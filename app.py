dccd65aef7ae4800d925215246679d70f0f8a8bf
Taketo Muroya <roomroof@gmail.com>
2022-08-12 15:08:28 +0900

    a

--- app.py
+++ app.py
@@ -289,7 +289,7 @@ y = ibc[228:]
 y = y.set_index('time')
 y.index = X[:len(ibc)-228].index
 ts = pd.merge(y, X, on='date')
-ts.index = ts.index.date()
+ts.index = ts.index.datetime.date()
 st.dataframe(ts)
 
 # データ期間の設定
